1678825c70c1c86a93b5fe8aadd594ba03adb887
nix-arif <nix.arif@gmail.com>
2022-01-27 07:53:25 +0800

    Extend class Readable from stream

---
+++ readable.js
@@ -0,0 +1,35 @@
+const { Readable } = require("stream");
+
+const advices = [
+  "No ice for drinks? Use frozen vegetables",
+  "If you feel alone, watch a horror movie before going to bed. You won't feel alone anymore.",
+  "Bon't have sex after chopping jalapones",
+  "If you can't blind them with brilliance, baffle them with nonsense",
+  "Always borrow money from a pessimist, they won't expect it back",
+];
+
+class StreamFromArray extends Readable {
+  constructor(array) {
+    super();
+    this.array = array;
+    this.index = 0;
+  }
+
+  _read() {
+    if (this.index <= this.array.length) {
+      const chunk = this.array[this.index];
+      this.push(chunk);
+      this.index += 1;
+    } else {
+      this.push(null);
+    }
+  }
+}
+
+const adviceStream = new StreamFromArray(advices);
+
+adviceStream.on("data", (chunk) => {
+  console.log(chunk);
+});
+
+adviceStream.on("end", () => console.log("Done!!!"));
